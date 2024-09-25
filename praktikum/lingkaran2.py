1d1f413de943629ef56d550154d373e0e7ac2f0c
Fortwikszz <triwibawa07@gmail.com>
2024-09-25 08:28:18 +0700

    9/25/24

--- praktikum/lingkaran2.py
+++ praktikum/lingkaran2.py
@@ -5,8 +5,8 @@ x1, y1 = int(x1), int(y1)
 xs, ys = int(xs), int(ys)
 xf, yf = int(xf), int(yf)
 
-jarling = ((xs-x1)**2+(ys-y1)**2)**0.5
-jarjal = abs((xf-xs)**2+(yf-ys)**2)**0.5
+jarling = abs((xf-x1)**2+(yf-y1)**2)
+jarjal = abs((xf-xs)**2+(yf-ys)**2)
 
 if jarling <= jarjal:
     print("No")
