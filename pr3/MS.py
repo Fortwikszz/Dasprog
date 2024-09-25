446562e9ad0a91c30417ef23c4cea6a3d8bb1163
Fortwikszz <triwibawa07@gmail.com>
2024-09-25 13:58:49 +0700

    9/25/24

--- pr3/MS.py
+++ pr3/MS.py
@@ -1,8 +1,8 @@
-n = int(input()) #input jumlah id sepatu yang terkena virus
+n = int(input())         #input jumlah id sepatu yang terkena virus
 
-enshoes = [None]*101 #inisiasi id sepatu yang terkena virus/enkripsi
-reshoes = [""]*101 #inisiasi id sepatu yang sudah dipulihkan/dekripsi
-match = 0 #inisisasi pasangan sepatu
+enshoes = [None]*100     #inisiasi id sepatu yang terkena virus/enkripsi
+reshoes = [""]*100       #inisiasi id sepatu yang sudah dipulihkan/dekripsi
+match = 0                #inisisasi pasangan sepatu
 
 for i in range(n):
     enshoes[i] = str(input()) #input id sepatu yang terenkripsi
@@ -17,7 +17,7 @@ for i in range(n):
     for j in range(i+1, n):
         if (f"{reshoes[i][:-1]}" == f"{reshoes[j][:-1]}") and (f"{reshoes[i][-1]}" != f"{reshoes[j][-1]}"): #jika angka pada ukuran sepatu sama dan huruf kapital berbeda(L/R)
             match += 1                                                                                      #maka menambah jumlah sepatu yang berpasangan
-            reshoes[j] = "0"                                                                                #dan mengganti salah satu variabel yang sama sehingga tidak terhitung
-                                                                                                            #kembali pada perulangan berikutnya
+            reshoes[j] = "0"                                                                                #dan mengganti salah variabel yang berpasangan sehingga tidak terhitung
+            reshoes[i] = "0"                                                                                #kembali pada perulangan berikutnya
        
 print(match) #output hasil jumlah sepatuyang berpasangan
